c9c11b3fb9efec8b31cb1eb015ad74a7f34ac549
blinkcat <1209311663@qq.com>
2019-11-29 17:29:48 +0800

    today

--- src/js/378.js
+++ src/js/378.js
@@ -3,7 +3,49 @@
  * @param {number} k
  * @return {number}
  */
-var kthSmallest = function(matrix, k) {};
+var kthSmallest = function(matrix, k) {
+    const nums = matrix[0].slice(0).map((v, i) => ({ x: 0, y: i, val: v }));
+
+    buildHeap();
+
+    for (let i = 0; i < k - 1; i++) {
+        const t = nums.shift();
+        if (t.x != matrix.length - 1) {
+            nums.unshift({ x: t.x + 1, y: t.y, val: matrix[t.x + 1][t.y] });
+        }
+        buildHeap();
+    }
+
+    return nums.shift().val;
+
+    function buildHeap() {
+        const len = nums.length;
+
+        for (let i = Math.floor(len / 2) - 1; i >= 0; i--) {
+            heapify(i);
+        }
+    }
+
+    // 小顶堆
+    function heapify(i) {
+        const l = i * 2 + 1;
+        const r = i * 2 + 2;
+        let mini = i;
+
+        if (l < nums.length && nums[l].val < nums[mini].val) {
+            mini = l;
+        }
+
+        if (r < nums.length && nums[r].val < nums[mini].val) {
+            mini = r;
+        }
+
+        if (mini !== i) {
+            [nums[i], nums[mini]] = [nums[mini], nums[i]];
+            heapify(mini, nums);
+        }
+    }
+};
 
 /**
  * 第一种方法，利用小顶堆。
@@ -17,7 +59,35 @@ var kthSmallest = function(matrix, k) {};
  * @param {number} k
  * @return {number}
  */
-var kthSmallest = function(matrix, k) {};
+var kthSmallest = function(matrix, k) {
+    const n = matrix.length;
+    let left = matrix[0][0];
+    let right = matrix[n - 1][n - 1];
+
+    while (left < right) {
+        let count = 0;
+        let mid = (left + right) >> 1;
+        let j = n - 1;
+
+        // 注意这里的优化，因为行和列都是递增。所以从行末开始
+        // 从右往左，从上到下。[i, j]大于mid，那么[i+1, j]，必然也大于mid。
+        // 所以从[i+1, j-1]开始
+        for (let i = 0; i < n; i++) {
+            while (j >= 0 && matrix[i][j] > mid) {
+                j--;
+            }
+            count += j + 1;
+        }
+
+        if (count < k) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }
+
+    return left;
+};
 
 /**
  * 第二种方法，二分查找
